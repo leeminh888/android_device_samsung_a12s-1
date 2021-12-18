c760e8d0e84d1f1cbc493e0b9c9d70352ac70638
leeminh888 <68751029+leeminh888@users.noreply.github.com>
2021-12-18 14:53:49 +0700

    Update twrp_a12s.mk

--- twrp_a12s.mk
+++ twrp_a12s.mk
@@ -21,7 +21,7 @@ PRODUCT_RELEASE_NAME := a12s
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/a12s/device.mk)
