15e70b32d882a4dde823421c06885470dc0d92ac
leeminh888 <68751029+leeminh888@users.noreply.github.com>
2021-12-18 14:36:33 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,4 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/samsung/a12s
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
